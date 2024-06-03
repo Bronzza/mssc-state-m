856a09952634925e44b8119a365edb1a57334e93
Serhii Repinetskyi <repinetskiy@gmail.com>
2024-06-03 09:48:52 +0700

    Inital commit

---
+++ src/main/java/com/example/mssc_state_m/MsscStateMApplication.java
@@ -0,0 +1,13 @@
+package com.example.mssc_state_m;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsscStateMApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsscStateMApplication.class, args);
+	}
+
+}
